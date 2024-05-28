d73a8aa1ca40b93a0007786ca94cbfd184046b62
Shane Shaji Thomas <shaneshaji10@gmail.com>
2024-05-28 15:35:54 +0530

    perf: convert file download to asynchronous

--- data_operations.py
+++ data_operations.py
@@ -1,42 +1,70 @@
-import os
-import requests
-from datetime import datetime, timedelta
+import aiohttp
+import asyncio
 from io import BytesIO
 from zipfile import ZipFile
+from datetime import datetime, timedelta
 import constants as c
+import os
 
-
-def download() -> None:
-    files_list = []
+async def extract(response, counter):
     cwd = os.getcwd()
+    resp = await response.read()
+    z = ZipFile(BytesIO(resp))
+    filename = z.namelist()[0]
+    z.extract(filename, f"{cwd}/DATA/90 DAYS")
+    path = f"{cwd}/DATA/90 DAYS/{filename}"
+    await rename(path)
+    if counter < 5:
+        z.extract(filename, f"{cwd}/DATA/5 DAYS")
+        path = f"{cwd}/DATA/5 DAYS/{filename}"
+        await rename(path)
+    if counter < 30:
+        z.extract(filename, f"{cwd}/DATA/30 DAYS")
+        path = f"{cwd}/DATA/30 DAYS/{filename}"
+        await rename(path)
+    
+async def rename(path):
+    date_str = path.split('cm')[1][:-8]
+    date_obj = datetime.strptime(date_str, "%d%b%Y")
+    new_path = path.split('cm')[0] + date_obj.strftime(c.DATE_FORMAT) + "-NSE-EQ.csv"
+    try:
+        os.rename(path, new_path)
+    except FileExistsError:
+        exit
+
+
+async def get_files():
+    cwd = os.getcwd()
+    current_date = datetime.today()-timedelta(days=1)
+    tasks = []
+    files_list = []
+    counter = 0
+    async with aiohttp.ClientSession() as session:
+        tasks = get_task(session)
+        responses = await(asyncio.gather(*tasks))
+        for response in responses:
+            if counter == 90:
+                break
+            if response.status==200:
+                await extract(response, counter)                
+                counter+=1
+            
+
+def get_task(session):
+    tasks = []
+    counter = 0
     current_date = datetime.today() - timedelta(days=1)
-    while len(files_list) <= 90:
+    while counter < 200:
         if current_date.weekday() < 5:
-
-            os.system('cls')
-            print(f"{len(files_list)+1}/90 files downloaded successfully.")
-
             url = f"{c.URL}{current_date.strftime('%Y')}/{current_date.strftime('%b').upper()}"
             date_string = current_date.strftime("%d%b%Y").upper()
             filename = f"cm{date_string}bhav.csv.zip"
             url = f"{url}/{filename}"
-
-            headers = {'Accept-Language': 'en-US,en;q=0.9,en-IN;q=0.8',
-                       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'}
-
-            response = requests.get(
-                url, stream=True, allow_redirects=False, headers=headers)
-            if response.status_code == 200:
-                z = ZipFile(BytesIO(response.content))
-                z.extractall(f"{cwd}/DATA/90 DAYS")
-                if len(files_list) < 5:
-                    z.extractall(f"{cwd}/DATA/5 DAYS")
-                if len(files_list) < 30:
-                    z.extractall(f"{cwd}/DATA/30 DAYS")
-                files_list.append(filename)
+            tasks.append(session.get(url, ssl = False))
         current_date = current_date - timedelta(days=1)
-    print(files_list)
+        counter=counter+1
 
+    return tasks
 
 if __name__ == "__main__":
-    download()
+    asyncio.run(get_files())
